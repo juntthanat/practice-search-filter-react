9495ba3ba0c32837877b665a15055cd6ff5110b9
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-05-27 14:18:12 +0700

    feat: create a search result container and fix shadow

--- practice-search-filter-react/src/App.jsx
+++ practice-search-filter-react/src/App.jsx
@@ -4,13 +4,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const testing = [
+    "num1",
+    "num2",
+    "num3"
+  ];
+
+
+
   return (
     <div className="App">
       <div className="back-ground">
         <div className="center-container">
-          <input className="search-box">
+          <input className="search-box" placeholder='What do you want to search?'></input>
+          <div className="result-container">
             
-          </input>
+          </div>
         </div>
       </div>
     </div>
